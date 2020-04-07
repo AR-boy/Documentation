91c143bef2e1f0f327d52e42db81541e255c5667
Matas Bartosevicius <m.bartosevicius@hotmail.co.uk>
2020-04-07 02:10:49 +0100

    Updates

---
+++ precache-manifest.276c912916459d9dbdc3efdbd53f2ef2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e032474befd6f8e93e420337b99aec29",
+    "url": "/plugin-documentation/index.html"
+  },
+  {
+    "revision": "0fdcb3f728e465fb8350",
+    "url": "/plugin-documentation/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "88e800532432b0b099b2",
+    "url": "/plugin-documentation/static/js/2.1ccea697.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/plugin-documentation/static/js/2.1ccea697.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fdcb3f728e465fb8350",
+    "url": "/plugin-documentation/static/js/main.11d25015.chunk.js"
+  },
+  {
+    "revision": "7c52517912a57adef212",
+    "url": "/plugin-documentation/static/js/runtime-main.8930cf34.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/plugin-documentation/static/media/logo.5d5d9eef.svg"
+  }
+]);
